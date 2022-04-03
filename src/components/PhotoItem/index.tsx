f76c0dc0510bbe22e60b4c1b5252bde6c4f4316b
Messias Viana <messiasdeviana@gmail.com>
2022-04-03 11:36:56 -0300

    Galeria quase finalizada

---
+++ src/components/PhotoItem/index.tsx
@@ -0,0 +1,19 @@
+import * as C from "./styles";
+import * as Photos from "../../services/photos";
+import { Photo } from "../../types/photo";
+
+type Props = {
+    url: string;
+    name: string;
+}
+
+export const PhotoItem = ({ url, name }: Props) => {
+    return(
+        <C.Container>
+            <img src={url} alt={name}/>
+            {name}
+            <input type="button" onClick={() => Photos.remove(name)} value="Excluir"/>
+        </C.Container>
+
+    )
+}
